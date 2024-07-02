1aa2135674f5fd66767b4c1b7deca8047716da93
yogesh192001 <146749002+yogesh192001@users.noreply.github.com>
2024-07-02 19:24:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ region_df = pd.read_csv("noc_regions.csv")
 df = preprocessor.preprocess(df, region_df)
 
 st.sidebar.header("Olympics Analysis")
-st.sidebar.image(r'C:\Users\sharm\Desktop\Project\Olympics_data-analysis\Olympic_logo.png')
+st.sidebar.image('https://e7.pngegg.com/pngimages/1020/402/png-clipart-2024-summer-olympics-brand-circle-area-olympic-rings-olympics-logo-text-sport.png')
 user_menu = st.sidebar.radio(
     "Select an Option",
     ('Medal Tally','Overall Analysis','Country-wise Analysis','Athlete wise Analysis')
